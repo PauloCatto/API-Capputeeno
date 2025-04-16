d3d4276eeacaf9379fbc3fee99a921a2e5fde251
Paulo Catto <paulohcatto@hotmail.com>
2025-04-15 23:17:48 -0300

    Fix error Cors for localhost

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-
 const { products } = require('./db');
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization'
