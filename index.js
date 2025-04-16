36648644f875e75904cc6924a592ac48f9a2feda
Paulo Catto <paulohcatto@hotmail.com>
2025-04-15 22:41:10 -0300

    Fix

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const express = require('express');
+const cors = require('cors');
 const { products } = require('./db');
 
-module.exports = (req, res) => {
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/api/products', (req, res) => {
   res.status(200).json({ products });
-};
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
