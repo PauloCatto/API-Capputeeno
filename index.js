918e04485d4694393051cea4498726a93742352d
Paulo Catto <paulohcatto@hotmail.com>
2025-04-15 21:44:40 -0300

    Fix settings

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const { products } = require('./db.js');
 const express = require('express');
 const app = express();
+const { products } = require('./db.js');
+const port = process.env.PORT || 3333;
 
 app.get('/products', (req, res) => {
   res.json(products);
+});
+
+app.listen(port, () => {
+  console.log(`API running on port ${port}`);
 });
 
 module.exports = app;
